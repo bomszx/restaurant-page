16c68948a738493bc2aee050488b2b402e954aaa
bon nocos <bon.nocos@gmail.com>
2022-08-24 00:47:52 +0800

    added nav part

--- src/header.js
+++ src/header.js
@@ -3,7 +3,20 @@ const header = () => {
 
     const home = document.createElement('button')
     home.innerText = 'Hello'
+
+    const menu = document.createElement('button')
+    menu.innerText = 'Menu'
+
+    const about = document.createElement('button')
+    about.innerText = 'About'
+
     element.append(home)
+    element.append(menu)
+    element.append(about)
+
+    element.classList.add('nav')
+
+    // Add Event Listener to each button
 
     return element
 }
