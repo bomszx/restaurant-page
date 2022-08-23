71d8cc0840d7ea3339aebf8dd588561b3d56c922
bon nocos <bon.nocos@gmail.com>
2022-08-23 16:04:04 +0800

    added inital layout

--- webpack.config.js
+++ webpack.config.js
@@ -15,6 +15,10 @@ module.exports = {
             {
                 test: /\.(png|svg|jpg|gif)$/i,
                 type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource'
             }
         ]
     }
